64298a63e11c8bcab944ea1fc65dab3e3a055320
S556370 <S556370@nwmissouri.edu>
2022-11-01 23:23:14 -0500

    Added route and view for selector, which renders set of 3 images using randomly generated  indexes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var universityRouter = require('./routes/University');
 var gridBuildRouter = require('./routes/gridBuild');
-
+var selectorRouter = require('./routes/selector');
 var app = express();
 
 // view engine setup
@@ -25,6 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/University', universityRouter);
 app.use('/gridBuild', gridBuildRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
