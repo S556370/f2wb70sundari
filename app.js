f6a247a10d16d91ed401b6d2eb4b8093d471f3d5
S556370 <S556370@nwmissouri.edu>
2022-11-08 08:05:05 -0600

    Misc : Changes in naming convention.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var universityRouter = require('./routes/university');
-var gridBuildRouter = require('./routes/gridBuild');
+var gridBuildRouter = require('./routes/gridbuild');
 var selectorRouter = require('./routes/selector');
 var app = express();
 
@@ -26,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.use('/university', universityRouter);
-app.use('/gridBuild', gridBuildRouter);
+app.use('/gridbuild', gridBuildRouter);
 app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
